538e40333a4f4372a0cbd12b022db629184aa2ca
jlcampbell16 <45213854+Jlcampbell16@users.noreply.github.com>
2019-03-13 16:24:34 -0600

    initial files. basic HTML, minimal CSS, initialized firebase, potential images saved

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Train-Scheduler
+
+This is a train schedule application that incorporates Firebase to host arrival and departure data. The website will provide up-to-date information about various trains, namely their arrival times and how many minutes remain until they arrive at their station.
---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDwTDRBHq9xSexVFPURzvsfkOzAXZoRbHU",
+    authDomain: "all-the-train-schedules.firebaseapp.com",
+    databaseURL: "https://all-the-train-schedules.firebaseio.com",
+    projectId: "all-the-train-schedules",
+    storageBucket: "all-the-train-schedules.appspot.com",
+    messagingSenderId: "924238295640"
+  };
+  firebase.initializeApp(config);
